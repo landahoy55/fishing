c05a45c46602b843a4333e6bbc3167ef88086daf
Pete <landahoy55@gmail.com>
2018-01-05 11:11:23 +0000

    add css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './Header.css';
 // <NavLink to="/edit" activeClassName="btn btn-success"><button type="button" className="btn btn-primary">Edit</button></NavLink>
 //<NavLink to="/session" activeClassName="btn btn-success"><button type="button" className="btn btn-primary">Start Session</button></NavLink>
 const Header = () => (
     <header>
         <h1>Gone Fishing</h1>
-        <NavLink to="/" activeClassName="btn btn-success" exact={true}><button type="button" className="btn btn-primary">Home</button></NavLink>
-        
-        <NavLink to="/location" activeClassName="btn btn-success"><button type="button" className="btn btn-primary">Start Session</button></NavLink>
-        <NavLink to="/live" activeClassName="btn btn-success"><button type="button" className="btn btn-primary">Live Sessions</button></NavLink>
+        <NavLink to="/" exact={true}><button type="button">Home</button></NavLink>
+        <NavLink to="/location"><button type="button">Start Session</button></NavLink>
+        <NavLink to="/live"><button type="button">Live Sessions</button></NavLink>
     </header>
 );
 
